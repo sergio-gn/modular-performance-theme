acda9ecf972a5559d78aabc6c3bf35de63cc1582
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2024-05-27 11:32:27 +1000

    Update header.php

--- header.php
+++ header.php
@@ -4,4 +4,5 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
+	<?php $gtag = get_field('google_tag','option'); if (isset($gtag) && !empty($gtag)){echo $gtag;}?>
 </head>
