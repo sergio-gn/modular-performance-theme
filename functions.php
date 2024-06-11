c90dcc79a20b56af3188f696ba96e5a5fb4e2369
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2024-06-12 07:10:37 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -72,19 +72,45 @@ function theme_support_admin_bar() {
 }
 add_action( 'after_setup_theme', 'theme_support_admin_bar' );
 
-function custom_buttons_shortcode() {
+function custom_buttons_shortcode($atts) {
+    // Define default attributes
+    $atts = shortcode_atts(
+        array(
+            'align' => 'center', // Default alignment
+        ), 
+        $atts, 
+        'custom_buttons'
+    );
+
+    // Determine the text-align value based on the attribute
+    $alignment = '';
+    switch ($atts['align']) {
+        case 'left':
+            $alignment = 'flex-start';
+            break;
+        case 'center':
+            $alignment = 'center';
+            break;
+        case 'right':
+            $alignment = 'flex-end';
+            break;
+        default:
+            $alignment = 'center';
+            break;
+    }
+
     ob_start(); ?>
     <div class="vc_empty_space"></div>
     <div class="wpb_text_column wpb_content_element">
         <div class="wpb_wrapper">
-            <div style="text-align: center; display: flex; gap: 10px;margin:1rem 0">
+            <div style="display: flex; justify-content: <?php echo esc_attr($alignment); ?>; gap: 10px; margin: 1rem 0;">
                 <a class="cta_button" href="tel:+<?php
-                        $phone_number = get_field('phone_number', 'option');
-                        // Remove any non-digit characters
-                        $phone_number_href = preg_replace('/\D/', '', $phone_number);
-                        // Ensure there are no spaces or other formatting
-                        echo $phone_number_href;
-                    ?>">
+                    $phone_number = get_field('phone_number', 'option');
+                    // Remove any non-digit characters
+                    $phone_number_href = preg_replace('/\D/', '', $phone_number);
+                    // Ensure there are no spaces or other formatting
+                    echo $phone_number_href;
+                ?>">
                     <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M16.5562 12.9062L16.1007 13.359C16.1007 13.359 15.0181 14.4355 12.0631 11.4972C9.10812 8.55901 10.1907 7.48257 10.1907 7.48257L10.4775 7.19738C11.1841 6.49484 11.2507 5.36691 10.6342 4.54348L9.37326 2.85908C8.61028 1.83992 7.13596 1.70529 6.26145 2.57483L4.69185 4.13552C4.25823 4.56668 3.96765 5.12559 4.00289 5.74561C4.09304 7.33182 4.81071 10.7447 8.81536 14.7266C13.0621 18.9492 17.0468 19.117 18.6763 18.9651C19.1917 18.9171 19.6399 18.6546 20.0011 18.2954L21.4217 16.883C22.3806 15.9295 22.1102 14.2949 20.8833 13.628L18.9728 12.5894C18.1672 12.1515 17.1858 12.2801 16.5562 12.9062Z" fill="#ffffff"/></g></svg>
                     <?php echo $phone_number; ?>
                 </a>
