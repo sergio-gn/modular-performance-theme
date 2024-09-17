32ade37120f6716903883548b83a925cea78d483
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2024-09-18 05:26:21 +1000

    Update two_cards.php

--- blocks/two_cards.php
+++ blocks/two_cards.php
@@ -4,10 +4,8 @@
             $image = get_sub_field('image');
             $titleCard1 = get_sub_field('title_card_1');
             $contentCard1 = get_sub_field('content_card_1');
-            $button = get_sub_field('button');
             $titleCard2 = get_sub_field('title_card_2');
             $contentCard2 = get_sub_field('content_card_2');
-            $button2 = get_sub_field('button_2');
             $bgcolour = get_sub_field('bg_colour');
             $bgcolour2 = get_sub_field('bg_colour_2');
         endwhile;
@@ -53,7 +51,7 @@
 <section class="d-flex_newcss justify-center_newcss blocks-padding">
     <div class="container">
         <div class="front-two_cards d-flex_newcss flex-d-column_mb justify-space-around gap_1">
-            <div class="front-two_cards_card two_cards_1 b-radius-1" style="background:<?php echo $bgcolour;?>">
+            <div class="front-two_cards_card two_cards_1 b-radius-1 <?php echo $bgcolour;?>">
                 <div class="two_cards_card_image d-flex align_center_blocks">
                     <div class="two_cards_1_content">
                         <?php if($contentCard1): ?>
@@ -72,7 +70,7 @@
                     </div>
                 </div>
             </div>
-            <div class="front-two_cards_card two_cards_2 b-radius-1" style="background:<?php echo $bgcolour2;?>">
+            <div class="front-two_cards_card two_cards_2 b-radius-1 <?php echo $bgcolour2 ?>">
                 <div class="fc-5 py-2">
                     <?php if($contentCard2): ?>
                         <?php echo $contentCard2; ?>
