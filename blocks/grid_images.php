323978de3217847f3b53f88a97352dcd49393e70
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2025-06-20 07:43:59 +1000

    Update grid_images.php

--- blocks/grid_images.php
+++ blocks/grid_images.php
@@ -14,13 +14,13 @@
     <?php if( have_rows('grid_images') ): ?>
         <div class="d-flex_newcss justify-center_newcss">
             <div class="container">
-                <div class="grid_square_images">
+                <div class="grid_square_images front-images-<?php echo $gridNumber; ?>">
                     <?php while( have_rows('grid_images') ): the_row(); 
                         $link = get_sub_field('link');
                         $image = get_sub_field('image');
                         $gridContent = get_sub_field('content');
                     ?>
-                        <div class="front-images-<?php echo $gridNumber; ?>">
+                        <div class="grid-item">
                             <?php if (!empty($link) && isset($link['url'])):?>
                                 <a href="<?php echo esc_url($link['url']); ?>">
                             <?php endif; ?>
